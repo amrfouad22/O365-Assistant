b3c2c06cee5e905b28ec71d391ac6cd83ece7506
Amr Fouad <amr.fouad@hotmail.co.uk>
2016-08-03 19:34:07 +1000

    v3 update to master

--- O365Constants.js
+++ O365Constants.js
@@ -3,7 +3,7 @@ module.exports={
     tenant : 'insightme.onmicrosoft.com',
     authorityUrl : 'https://login.windows.net/51ef0b43-0b60-47a7-9872-e7c929542973',
     clientId : 'b65937cd-73fc-41af-8ba2-7f9415b5ad57',
-    clientSecret : 'Your App Secret',
+    clientSecret : 'm5YlJswhTa2AowvSrR0/vCChhV4D3drNt2FlwY3ERm8=',
     resource : 'https://graph.microsoft.com',
     bookMeetingUrl:'https://graph.microsoft.com/v1.0/users/amrfouad@insightme.onmicrosoft.com/calendar/events'
 }
