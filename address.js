0f43137804d98bbb64bac087db8bf9ae3e14c7fb
Amr Fouad <amr.fouad@hotmail.co.uk>
2018-05-15 17:37:39 +1000

    update address

--- address.js
+++ address.js
@@ -1,12 +1,14 @@
 module.exports = 
-{ id: '1510560827617',
-channelId: 'msteams',
-user:
- { id: '29:1I0KZ6zRkJVZSjFO0gOfivbRitOgxF8t61fwm989q7a4Q9ePntfBqZcslYokrTdCneVOwUZpgWfsLuP82Q9U3Hg',
-   name: 'Alex Wilber',
-   aadObjectId: '86401569-3854-401c-b705-da458e1d42a0' },
-conversation: { id: 'a:1i6MvCEzHZbY3EKIslhB0LDCYxX4AB02-bgDuAFCa2t7pp020p4sT65KtWN7ZvLQMgKWhIT-5DDvpDlwJk-8AkhvyDm5hO7B_osY8ag4Qrfg4sn2Z7rEHTzqSIo1VDxxV' },
-bot:
- { id: '28:be77fcaa-5e0f-431c-9a2a-76d7eadfccc2',
-   name: 'Office Assistant Demo' },
-serviceUrl: 'https://smba.trafficmanager.net/amer-client-ss.msg/' }
+{ id: '1526342377258',
+  channelId: 'msteams',
+  user:
+   { id: '29:1vEEbongeHW7wo4WhWaE9ScIc2nBZ-m2Jhf08jrzORhjq3oOHzYtUBWS2fqiaYimSPC9HReStt02S6U95vZ2uOw',
+     name: 'Amr Fouad',
+     aadObjectId: 'ea96e3b5-78b5-4319-94a8-da48b9e3c2cf' },
+  conversation:
+   { conversationType: 'personal',
+     id: 'a:1crturwnE6Zk-iroJDe_Qu2QTLabSadTOBzUtNeIcLS-4U5YwGgEGGlKx_UZEi4dwzn1iLkycNCT0qeLEOAEU1sav-zfRAcOBYBLmFERPzmY0vu4R0Jgt13rjRqGjNOiB' },
+  bot:
+   { id: '28:be77fcaa-5e0f-431c-9a2a-76d7eadfccc2',
+     name: 'Office Assistant Demo' },
+  serviceUrl: 'https://smba.trafficmanager.net/emea-client-ss.msg/' }
